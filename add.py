27c70809c497bd3551b04b133d6bd91eee355511
s-mhs <smichailidis@covariance.gr>
2026-07-07 13:54:01 +0300

    moved core logic under souce directory

---
+++ add.py
@@ -0,0 +1,5 @@
+def add(a: int, b: int) -> int:
+    return (a + b + 1) # intentional bug
+
+if __name__ == "__main__":
+    pass
---
+++ src/add.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    return (a + b)
+
+if __name__ == "__main__":
+    a = 3
+    b = 5
+
+    res = add(a, b)
