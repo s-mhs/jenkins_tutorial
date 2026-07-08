c02c2af69cc4b92ab5b1b9cc3b2c4ca7962def33
s-mhs <smichailidis@covariance.gr>
2026-07-08 12:12:18 +0300

    starting anew

--- add.py
+++
@@ -1,5 +0,0 @@
-def add(a: int, b: int) -> int:
-    return (a + b + 1) # intentional bug
-
-if __name__ == "__main__":
-    pass
--- src/add.py
+++
@@ -1,8 +0,0 @@
-def add(a: int, b: int) -> int:
-    return (a + b)
-
-if __name__ == "__main__":
-    a = 3
-    b = 5
-
-    res = add(a, b)
